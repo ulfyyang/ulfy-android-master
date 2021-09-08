f7f65976a90093b582169045ae1836c215aa597e
spark <>
2021-09-08 20:29:38 +0800

    MagicIndicator增加图像指示器支持

--- app/src/main/java/com/ulfy/master/ui/view/TabPager2View.java
+++ app/src/main/java/com/ulfy/master/ui/view/TabPager2View.java
@@ -11,14 +11,12 @@ import com.ulfy.android.mvvm.IViewModel;
 import com.ulfy.android.ui_injection.Layout;
 import com.ulfy.android.ui_injection.ViewById;
 import com.ulfy.android.ui_linkage.MagicTabPagerLinkage;
-import com.ulfy.android.ui_linkage.OnTabSelectedListener;
 import com.ulfy.android.utils.UiUtils;
 import com.ulfy.master.R;
 import com.ulfy.master.application.vm.TabPager2VM;
 import com.ulfy.master.ui.base.BaseView;
 
 import net.lucode.hackware.magicindicator.MagicIndicator;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
 
 @Layout(id = R.layout.view_tab_pager2)
 public class TabPager2View extends BaseView {
@@ -46,6 +44,7 @@ public class TabPager2View extends BaseView {
             1. 显示容器只支持 ViewPager
             2. 标签按钮只支持字符串
             3. 标签页支持 View 和 Fragment
+            4. 下划线和图像指示器只能选一种
      */
     private void init(Context context, AttributeSet attrs) {
         linkage.setMagicIndicator(tabsMI).setContainer(containerVP)
@@ -55,17 +54,18 @@ public class TabPager2View extends BaseView {
                 .setTitleSelectedColor(Color.parseColor("#f57c00"))           // 设置标签选中文字颜色
                 .setTitleScale(false)                                                   // 设置标签文字是否有大小缩放效果
                 .setTitleBold(true)                                                     // 设置是否字体加粗
-                .setIndicatorBounce(false)                                              // 设置是否有弹性回滚效果
-                .setIndicatorMode(LinePagerIndicator.MODE_WRAP_CONTENT)                 // 设置标签下划线的宽度模式
-                .setIndicatorWidthDP(20)                                                // 设置指示器线条宽度，设置 MODE_EXACTLY 生效
-                .setIndicatorHeightDP(2)                                                // 设置指示器线条的高度
-                .setIndicatorYOffsetDp(0)                                               // 设置指示器线条距离文字的偏移量
-                .setIndicatorColor(Color.parseColor("#f57c00"))               // 设置指示器线条的颜色
-                .addOnTabSelectedListener(new OnTabSelectedListener() {                 // 当标签页被选中时回调
-                    @Override public void onTabSelected(int index) {
-                        UiUtils.show("选中了标签页：" + index);
-                    }
-                })
+//                .setIndicatorBounce(false)                                              // 设置是否有弹性回滚效果
+//                .setIndicatorMode(LinePagerIndicator.MODE_WRAP_CONTENT)                 // 设置标签下划线的宽度模式
+//                .setIndicatorWidthDP(20)                                                // 设置指示器线条宽度，设置 MODE_EXACTLY 生效
+//                .setIndicatorHeightDP(2)                                                // 设置指示器线条的高度
+//                .setIndicatorYOffsetDp(0)                                               // 设置指示器线条距离文字的偏移量
+//                .setIndicatorColor(Color.parseColor("#f57c00"))               // 设置指示器线条的颜色
+                .setIndicatorResource(R.drawable.shape_shadow)                          // 设置指示器图像
+                .setIndicatorPaddingDp(8)                                              // 设置指示器图像到内容的内边距
+                .setIndicatorPaddingLeftDp(12)      // 因为阴影占了一部分宽度，所以重新设置下方三个边的边距
+                .setIndicatorPaddingRightDp(12)
+                .setIndicatorPaddingBottonDp(12)
+                .addOnTabSelectedListener(index -> UiUtils.show("选中了标签页：" + index))// 当标签页被选中时回调
         ;
     }
 
