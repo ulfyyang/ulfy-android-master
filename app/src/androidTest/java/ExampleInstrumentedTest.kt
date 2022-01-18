f655a22ed250c67853122679bed2ecdbecf5d43b
spark <>
2022-01-18 23:05:32 +0800

    编写卸载老旧App使用示例

--- app/src/androidTest/java/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ExampleInstrumentedTest.kt
@@ -1,15 +1,63 @@
 package com.ulfy.master
 
+import android.content.Intent
+import android.net.Uri
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.ulfy.extra.System
+import com.ulfy.master.ui.activity.List1Activity
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Exception
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("com.ulfy.master", appContext.packageName)
+
+}
+
+/**
+ * 测试卸载指定的App
+ */
+@RunWith(AndroidJUnit4::class)
+class TestUninstallApp {
+    private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+    /*
+    拆分步骤测试
+     */
+
+    @Test fun checkAppInstalled() { // 检查一个app是否安装了
+        val packageName = "com.ulfy.master"
+//        val packageName = "com.ulfy.animal"
+        val find = try {
+            appContext.packageManager.getPackageInfo(packageName, 0)
+            true
+        } catch (e: Exception) {
+            false
+        }
+        Assert.assertTrue(find)
+    }
+
+    @Test fun uninstallApp() {      // 以弹出框提示方式卸载app
+        // <uses- Manifest.permission android:name="android.permission.REQUEST_DELETE_PACKAGES"/>    <!--适配9.0卸载问题-->
+        // Intent.FLAG_ACTIVITY_NEW_TASK 在非Activity环境中启动Activity
+        val packageName = "com.ulfy.master"
+        val intent = Uri.fromParts("package", packageName, null)
+            .run { Intent(Intent.ACTION_DELETE, this).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+        appContext.startActivity(intent)
+    }
+
+    /*
+    主步骤测试：必须在清单文件中添加以下权限
+    <uses- Manifest.permission android:name="android.permission.REQUEST_DELETE_PACKAGES"/>    <!--适配9.0卸载问题-->
+     */
+    @Test fun uninstallAppByMessage() {
+        val packageNames = arrayOf("", "com.ulfy.animal", "com.ulfy.master", "com.google.android.youtube")
+        val message = "发现旧安装包，是否删除？"
+        ActivityScenario.launch(List1Activity::class.java).onActivity {
+            System.uninstallApp(it, packageNames, message)
+        }.result.resultCode
     }
 }
---
+++ app/src/main/java/com/ulfy/extra/ExtraUtil.kt
@@ -0,0 +1,50 @@
+package com.ulfy.extra
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import com.ulfy.android.dialog.DialogUtils
+import java.lang.Exception
+
+object System {
+
+    /**
+     * 卸载多个App，每次卸载时弹出一个提示(由于卸载过程是异步过程，因此只会卸载列表中找到的第一个App)
+     * 必须在清单文件中添加以下权限
+     * <uses- Manifest.permission android:name="android.permission.REQUEST_DELETE_PACKAGES"/>    <!--适配9.0卸载问题-->
+     * @param context 上下文，必须是Activity上下文
+     * @param packageNames 卸载的App包名数组
+     * @param message 卸载之前弹出的确认提示框文字
+     */
+    fun uninstallApp(context: Context, packageNames: Array<String>?, message: String) {
+        if (context !is Activity) {
+            throw Exception("context must be a activity context!")
+        }
+        if (packageNames == null || packageNames.isEmpty()) {
+            return
+        }
+        packageNames.filter { it.isNotEmpty() }.firstOrNull { it.checkAppInstalled(context) }?.let {
+            showTipThenDo(context, message) {
+                it.uninstallApp(context)
+            }
+        }
+    }
+
+    private fun showTipThenDo(context: Context, message: String, runnable: Runnable) {
+        DialogUtils.showAlertTwoButtonDialog(context, "卸载应用", message) {
+            runnable.run()
+        }
+    }
+    private fun String.checkAppInstalled(context: Context) = try {
+        context.packageManager.getPackageInfo(this, 0)
+        true
+    } catch (e: Exception) {
+        false
+    }
+    private fun String.uninstallApp(context: Context) {
+        val intent = Uri.fromParts("package", this, null)
+            .run { Intent(Intent.ACTION_DELETE, this) }
+        context.startActivity(intent)
+    }
+}
