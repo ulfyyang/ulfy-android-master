be508a930705fa287cac28dfd573a439fc343b72
spark <>
2022-01-18 20:41:06 +0800

    配置demo支持kotlin

--- app/src/androidTest/java/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ExampleInstrumentedTest.kt
@@ -1,26 +1,15 @@
-package com.ulfy.master;
+package com.ulfy.master
 
-import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.ulfy.starter", appContext.getPackageName());
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.ulfy.master", appContext.packageName)
     }
 }
--- app/src/test/java/ExampleUnitTest.kt
+++ app/src/test/java/ExampleUnitTest.kt
@@ -1,17 +1,8 @@
-package com.ulfy.master;
+package com.ulfy.master
 
-import org.junit.Test;
+import org.junit.Assert
+import org.junit.Test
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+class ExampleUnitTest {
+    @Test fun addition_isCorrect() = Assert.assertEquals(4, (2 + 2).toLong())
 }
